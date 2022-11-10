198988243d998be683f9be4c70ea48d573949775
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-10 15:13:12 +0100

    dato un prezzo per il biglietto ripetto ai km

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 /*---Inserimento Dati---*/
 let numeroKm = prompt("inserisci quanti chilometri vuoi percorrere!");
 let età = prompt("inserisci la tua età!");
+
+/*---Prezzo biglietto normale-----*/
+let prezzoBglietto = (numeroKm * 0.21);
