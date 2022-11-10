8285f5fd7937b9a916f257638a774ee9acf6d398
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-10 16:09:36 +0100

    il biglietto stampato sulla console

--- script.js
+++ script.js
@@ -11,6 +11,52 @@ let sconto2 = 40/100 ;
 //
 let scontoMinori = (prezzoBglietto - sconto1);
 let scontoOver65 = (prezzoBglietto - sconto2);
-/**/ 
 
 
+/*---Biglietto stampato con prezzo rispetto all'età*/ 
+if(età <= 0 || età >= 120 ){
+console.log("Scrivi un'età giusta!!!!");
+
+}else if(età < 18){
+    console.log(
+        `
+        Chilometri da Percorrere = ${numeroKm}
+
+        La tua età è = ${età}
+
+--------------------------------------------------------
+
+        Prezzo finale di = ${scontoMinori} €
+        `
+    );
+    
+} else if(età > 65) {
+
+    console.log(
+        `
+        Chilometri da Percorrere = ${numeroKm}
+
+        La tua età è = ${età}
+
+--------------------------------------------------------
+
+        Prezzo finale di = ${scontoOver65} €
+
+        `
+    );
+    
+}else{
+    console.log(
+        `
+        Chilometri da Percorrere = ${numeroKm}
+
+        La tua età è = ${età}
+
+--------------------------------------------------------
+
+        Prezzo finale di = ${prezzoBglietto} €
+
+        `
+    );
+
+};
