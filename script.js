ef9d734bb8c9012ac5180d82978fde1bceb1d20d
DrLiveasy <youssef.saleh.hans@gmail.com>
2022-11-10 15:31:43 +0100

    dato i valori agli sconti

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ let età = prompt("inserisci la tua età!");
 
 /*---Prezzo biglietto normale-----*/
 let prezzoBglietto = (numeroKm * 0.21);
+
+/*---i valori per lo sconto---*/
+let sconto1 = 20/100 ;
+let sconto2 = 40/100 ;
+//
+let scontoMinori = (prezzoBglietto - sconto1);
+let scontoOver65 = (prezzoBglietto - sconto2);
+/**/ 
+
+
